7ca5dadc9bbe30bbb41b6878af94398bb9221c50
SaadJav <155204576+SaadJav@users.noreply.github.com>
2023-12-30 00:59:50 +0000

    Create app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+ let menuIcon = document.querySelector(".humburger")
+
+        menuIcon.addEventListener("click", () => {
+            let menuBtn = document.querySelector(".navmenu");
+            menuBtn.classList.toggle("active")
+        })
